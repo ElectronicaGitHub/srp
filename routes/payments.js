a764ae1d2c7e0a0b3c72fdbc16092edd9341aa6f
Philip <molo4nik11@gmail.com>
2016-03-25 14:19:12 +0300

    check

--- routes/payments.js
+++ routes/payments.js
@@ -1,6 +1,7 @@
 var shopId = 51180;
 var scid = 533684;
 var paymentUrl = 'https://demomoney.yandex.ru/eshop.xml';
+var Request = require('../models/Request.js');
 
 module.exports = function (express) {
 	var router = express.Router();
@@ -24,7 +25,21 @@ module.exports = function (express) {
 
 	router.post('/demoaviso', function (req, res, next) {
 		console.log('demoaviso', req.body);
-		send(0);
+		var data = {
+			performedDatetime : req.body.requestDatetime,
+			invoiceId : req.body.invoiceId, 
+			shopId : req.body.shopId,
+			code : 0
+		};
+		
+		var xmlbody = '<paymentAvisoResponse performedDatetime="' + data.performedDatetime + 
+						'" code="' + data.code + 
+						'" invoiceId="' + data.invoiceId + 
+						'" shopId="' + data.shopId + '"/>';
+
+		Request.findByIdAndUpdate(req.body.orderNumber, { status : 2 }, function (err, result) {
+			res.end(xmlbody);
+		});
 	});
 
 	router.post('/check', function (req, res, next) {
