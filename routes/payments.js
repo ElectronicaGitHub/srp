6167f8bfd6ea906ad0cd2f6dfb52e5721ede840c
Philip <molo4nik11@gmail.com>
2016-04-21 18:28:10 +0300

    payment robokassa

--- routes/payments.js
+++ routes/payments.js
@@ -3,84 +3,127 @@ var scid = 533684;
 var paymentUrl = 'https://demomoney.yandex.ru/eshop.xml';
 var Request = require('../models/Request.js');
 
-module.exports = function (express) {
+var robokassa = {
+	env : {
+		prod : {
+			alg : 'MD5',
+			pass1 : 'Cr1jM4R4aBqtu44dHNjL',
+			pass2 : 'ok60uz6DdquqDHF7rL1V'
+		},
+		test : {
+			alg : 'MD5',
+			pass1 : 'Q3mYKRLEpNnO96Hz8mQ7',
+			pass2 : 'lv0N2J1ozoDMi9ad0fxo'
+		},
+	},
+	urls : {
+		methods : 'POST, POST, POST',
+		result : 'serpantin.moscow/result',
+		success : 'serpantin.moscow/success',
+		fail : 'serpantin.moscow/fail'
+	}
+}
+module.exports = function  (express) {
 	var router = express.Router();
 
-	// TEST HANDLERS
-
-	router.post('/democheck', function (req, res, next) {
-		console.log('democheck', req.body);
-		var data = {
-			performedDatetime : req.body.requestDatetime,
-			invoiceId : req.body.invoiceId, 
-			shopId : req.body.shopId,
-			code : 0
-		};
-
-		var xmlbody = '<checkOrderResponse performedDatetime="' + data.performedDatetime + 
-						'" code="' + data.code + 
-						'" invoiceId="' + data.invoiceId + 
-						'" shopId="' + data.shopId + '"/>';
-
-		res.end(xmlbody);
+	router.get('/success', function (req, res next) {
+		// Request.findByIdAndUpdate(req.body.orderNumber, { status : 2 }, function (err, result) {
+		// res.json({
+			// message : 'ok'
+		// })
+		// });
+		res.send('1');
 	});
 
-	router.post('/demoaviso', function (req, res, next) {
-		console.log('demoaviso', req.body);
-		var data = {
-			performedDatetime : req.body.requestDatetime,
-			invoiceId : req.body.invoiceId, 
-			shopId : req.body.shopId,
-			code : 0
-		};
 
-		var xmlbody = '<paymentAvisoResponse performedDatetime="' + data.performedDatetime + 
-						'" code="' + data.code + 
-						'" invoiceId="' + data.invoiceId + 
-						'" shopId="' + data.shopId + '"/>';
-
-		Request.findByIdAndUpdate(req.body.orderNumber, { status : 2 }, function (err, result) {
-			res.end(xmlbody);
-		});
-	});
-
-	// REAL HANDLERS
-
-	router.post('/check', function (req, res, next) {
-		var data = {
-			performedDatetime : req.body.requestDatetime,
-			invoiceId : req.body.invoiceId, 
-			shopId : req.body.shopId,
-			code : 0
-		};
-
-		var xmlbody = '<checkOrderResponse performedDatetime="' + data.performedDatetime + 
-						'" code="' + data.code + 
-						'" invoiceId="' + data.invoiceId + 
-						'" shopId="' + data.shopId + '"/>';
-
-		res.end(xmlbody);
-	});
-
-	router.post('/aviso', function (req, res, next) {
-		var data = {
-			performedDatetime : req.body.requestDatetime,
-			invoiceId : req.body.invoiceId, 
-			shopId : req.body.shopId,
-			code : 0
-		};
-
-		var xmlbody = '<paymentAvisoResponse performedDatetime="' + data.performedDatetime + 
-						'" code="' + data.code + 
-						'" invoiceId="' + data.invoiceId + 
-						'" shopId="' + data.shopId + '"/>';
-
-		Request.findByIdAndUpdate(req.body.orderNumber, { status : 2 }, function (err, result) {
-			res.end(xmlbody);
-		});
-	});
+	router.get('/fail', function (req, res, next) {
+		res.send('0');
+	})
 
 
 
 	return router;
+	
 }
+
+// module.exports = function (express) {
+// 	var router = express.Router();
+
+// 	// TEST HANDLERS
+
+// 	router.post('/democheck', function (req, res, next) {
+// 		console.log('democheck', req.body);
+// 		var data = {
+// 			performedDatetime : req.body.requestDatetime,
+// 			invoiceId : req.body.invoiceId, 
+// 			shopId : req.body.shopId,
+// 			code : 0
+// 		};
+
+// 		var xmlbody = '<checkOrderResponse performedDatetime="' + data.performedDatetime + 
+// 						'" code="' + data.code + 
+// 						'" invoiceId="' + data.invoiceId + 
+// 						'" shopId="' + data.shopId + '"/>';
+
+// 		res.end(xmlbody);
+// 	});
+
+// 	router.post('/demoaviso', function (req, res, next) {
+// 		console.log('demoaviso', req.body);
+// 		var data = {
+// 			performedDatetime : req.body.requestDatetime,
+// 			invoiceId : req.body.invoiceId, 
+// 			shopId : req.body.shopId,
+// 			code : 0
+// 		};
+
+// 		var xmlbody = '<paymentAvisoResponse performedDatetime="' + data.performedDatetime + 
+// 						'" code="' + data.code + 
+// 						'" invoiceId="' + data.invoiceId + 
+// 						'" shopId="' + data.shopId + '"/>';
+
+// 		Request.findByIdAndUpdate(req.body.orderNumber, { status : 2 }, function (err, result) {
+// 			res.end(xmlbody);
+// 		});
+// 	});
+
+// 	// REAL HANDLERS
+
+// 	router.post('/check', function (req, res, next) {
+// 		var data = {
+// 			performedDatetime : req.body.requestDatetime,
+// 			invoiceId : req.body.invoiceId, 
+// 			shopId : req.body.shopId,
+// 			code : 0
+// 		};
+
+// 		var xmlbody = '<checkOrderResponse performedDatetime="' + data.performedDatetime + 
+// 						'" code="' + data.code + 
+// 						'" invoiceId="' + data.invoiceId + 
+// 						'" shopId="' + data.shopId + '"/>';
+
+// 		res.end(xmlbody);
+// 	});
+
+// 	router.post('/aviso', function (req, res, next) {
+// 		var data = {
+// 			performedDatetime : req.body.requestDatetime,
+// 			invoiceId : req.body.invoiceId, 
+// 			shopId : req.body.shopId,
+// 			code : 0
+// 		};
+
+// 		var xmlbody = '<paymentAvisoResponse performedDatetime="' + data.performedDatetime + 
+// 						'" code="' + data.code + 
+// 						'" invoiceId="' + data.invoiceId + 
+// 						'" shopId="' + data.shopId + '"/>';
+
+// 		Request.findByIdAndUpdate(req.body.orderNumber, { status : 2 }, function (err, result) {
+// 			res.end(xmlbody);
+// 		});
+// 	});
+
+
+
+// 	return router;
+// }
