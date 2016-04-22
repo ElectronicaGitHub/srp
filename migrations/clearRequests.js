a6366aaf73211bc4732c8e084c605201f4a691ec
Philip <molo4nik11@gmail.com>
2016-04-22 14:09:12 +0300

    pay

---
+++ migrations/clearRequests.js
@@ -0,0 +1,24 @@
+var InvId = require('../models/InvId.js');
+var User = require('../models/User.js');
+var Request = require('../models/Request.js');
+var async = require('async');
+
+async.series([
+	function (callback) {
+		Request.remove({}, callback);
+	},
+	function (callback) {
+		User.find({}, function (err, results) {
+			console.log('user.length = ', results.length);
+			async.each(results, function (doc) {
+				doc.reqests = [];
+				doc.save();
+			});
+		});
+	}
+], function (err) {
+	if (err) callback(err);
+	console.log('МИГРАЦИЯ ПРОШЛА УСПЕШНО');
+})
+
+
